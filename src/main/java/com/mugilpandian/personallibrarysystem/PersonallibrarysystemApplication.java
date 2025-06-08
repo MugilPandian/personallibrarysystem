e6ec8409ddc9f56eb1f9874639ae4097a89585c0
Mugil Pandian V <mugilpandianrn@gmail.com>
2025-06-08 20:24:08 +0530

    Initial Commit

---
+++ src/main/java/com/mugilpandian/personallibrarysystem/PersonallibrarysystemApplication.java
@@ -0,0 +1,13 @@
+package com.mugilpandian.personallibrarysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonallibrarysystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonallibrarysystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mugilpandian/personallibrarysystem/PersonallibrarysystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mugilpandian.personallibrarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonallibrarysystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
